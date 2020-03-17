5835fd7d1d9c6230995460a5a33e87e622ccbd1e
Tommy So <sofuming@gmail.com>
2020-03-17 17:28:44 +0800

    add comments to data.sql and properties

--- src/main/resources/data-h2.sql
+++ src/main/resources/data-h2.sql
@@ -1,3 +1,6 @@
+-- Product types were hard-coded in in-memory h2 database
+-- It should be done in normal SQL or NoSQL database in real-world case
+
 INSERT INTO item(product_type, product_name) VALUES('Stationary', 'book');
 INSERT INTO item(product_type, product_name) VALUES('Stationary', 'pencils');
 INSERT INTO item(product_type, product_name) VALUES('Food', 'potato chips');
